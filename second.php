0d569d51abc0d46694a25e3ff98e3bd646f5472b
midhunflydux <midhun@flydux.com>
2018-12-12 16:06:46 +0530

    This is the Second commit of midhun_git

---
+++ second.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>The second file header</h1>
+    <form class="test" action="index.html" method="post">
+      <input type="text" name="name" value="">
+      <input type="text" name="age" value="">
+      <input type="number" name="number" value="">
+    </form>
+  </body>
+</html>
+<?php echo "This is the second file";  ?>
